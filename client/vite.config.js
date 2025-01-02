174f70f3c9854188dd795627f80dcd14d9bae01a
Prabhudatta19 <prabhudatta.prof@gmail.com>
2025-01-02 23:00:59 +0530

    vite config for build

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-        '/api': {
-            target: 'http://localhost:5000',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ''),
+    plugins: [react()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
         },
     },
-},
+    root: '.', // Ensure this points to the correct root (default is the directory of vite.config.js)
+    build: {
+        outDir: 'dist', // Output directory for the build files
+    },
 })
